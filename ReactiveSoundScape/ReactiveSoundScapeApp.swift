98f6371fe162e617ce02aeca28ecf269867ba2dd
Roy <Braks>
2023-03-28 14:10:16 +0200

    Initial Commit

---
+++ ReactiveSoundScape/ReactiveSoundScapeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReactiveSoundScapeApp.swift
+//  ReactiveSoundScape
+//
+//  Created by Roy Braks on 28/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct ReactiveSoundScapeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
